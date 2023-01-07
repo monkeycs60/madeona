4aff380a366c6f70ac19662c5f849908b8b1b947
monkeycs60 <clement.serizay@gmail.com>
2023-01-07 03:05:01 +0100

    cool

--- pages/index.js
+++ pages/index.js
@@ -12,15 +12,42 @@ const HelloWorld = () => {
         />
         <title>Madeona Photography</title>
       </Head>
+
       <h1>Hello World</h1>
-      <div className="container w-[100vw] h-[80vh] relative">
+
+       <div className="m-10 flex w-[50vw] h-[40vh] relative"> 
         <Image
-          src="/chien-mignon-compressed2.webp"
-          alt="Picture of the author"
-          priority
-          fill
+          src="/chien-mignon-compressed1-256px.png"
+          alt="little dog"
+          priority={true}
+          width="100"
+          height="100"
         />
       </div>
+
+
+       {/* <div className="m-10 flex xl:hidden 2xl:hidden w-[50vw] h-[40vh] relative"> 
+        <Image
+          src="/chien-mignon-compressed1-256px.png"
+          alt="little dog"
+          priority={true}
+          width="100"
+          height="100"
+        />
+      </div>
+      <div className="m-10 hidden xl:flex 2xl:flex w-[30vw] h-[50vh] relative">
+        <Image
+          src="/chien-mignon-compressed1-2048px.png"
+          alt="big one"
+          priority={true}
+          width="1200"
+          height="300"
+        />
+      </div>  */}
+
+
+  
+
       <p>hello la famille</p>
       <p>
         Coucou les bouffons Lorem ipsum dolor sit amet, consectetur adipisicing
